7a80851db1ce9d114f677973bce89955ca2673e1
ikoojoshi <ipshitamjoshi@gmail.com>
2020-09-19 20:42:12 +0530

    minor changes in game

--- app.py
+++ app.py
@@ -18,10 +18,23 @@ def essentials():
 def entertainment():
     return render_template("entertainment.html")
 
+#entertainment
+@app.route('/gdino')
+def gdino():
+    return render_template("g-dino.html")
+
+@app.route('/ggame')
+def ggame():
+    return render_template("g-game.html")
+
+
+#essentials
 @app.route('/email')
 def email():
     return render_template("e-mail.html")
 
+
+#appliances
 @app.route('/anavigation')
 def anavigation():
     return render_template("a-navigation.html")
